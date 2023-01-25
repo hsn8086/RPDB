d8093d40c380f648140dc83fadc230325087a7c3
hsn2 <912372447@qq.com>
2023-01-25 15:39:08 +0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='RPDB',
+    version='1.0.1',
+    packages=[''],
+    url='https://github.com/hsn8086/RPDB',
+    license='MIT license',
+    author='hsn8086',
+    author_email='hsn1919810@gmail.com',
+    description=''
+)
